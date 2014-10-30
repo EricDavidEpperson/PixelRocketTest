2456a61c606b3bef36e4e5588e9db1bb6e64d656
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-10-30 13:45:30 -0500

    passing all supplied test, additional pp tests, fails last new test

--- phone.rb
+++ phone.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Phone
+  attr_accessor :number
 
   def initialize(number)
     @number = number
@@ -8,6 +9,20 @@ class Phone
   end
 
   def number
+    number_is_right_length(@number)
+  end
+
+  def area_code
+    number_is_right_length(@number)
+    @number = @number[0, 3]
+  end
+
+  def to_s
+    number_is_right_length(@number)
+    @number = "(" + @number[-10, 3] + ")" + " " + @number[-7, 3] + "-" + @number[-4, 4]
+  end
+
+  def number_is_right_length(number)
     case @number.length
     when 10
       return @number
@@ -19,13 +34,5 @@ class Phone
     else
       @number ="0000000000"
     end
-  end
-
-  def area_code
-    @number = @number[0, 3]
-  end
-
-  def to_s
-    @number = "(" + @number[-10, 3] + ")" + " " + @number[-7, 3] + "-" + @number[-4, 4]
   end
 end
